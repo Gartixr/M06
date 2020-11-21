58c98a0eb14c313f8a6285e71105d4a057804436
Gartixr <adidumitrica@gmail.com>
2020-11-21 22:57:09 +0100

    Finalizar clase Vehicle

--- DAM-M06/src/uf2/exercici08/manteniment/Vehicle.java
+++ DAM-M06/src/uf2/exercici08/manteniment/Vehicle.java
@@ -1,9 +1,57 @@
 package uf2.exercici08.manteniment;
 
 import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 public class Vehicle implements Serializable{
 
+	private static final long serialVersionUID = 1;
 	
+	@Id @GeneratedValue
+	private long id;
+	
+	private Date dataImportacio;
+	private String nomModel;
+	private double preu;
+	private boolean arreglat;
+	
+	public Vehicle(){
+		
+	}
+	
+	public Vehicle(Date dataImportacio, String nomModel, double preu, boolean arreglat){
+		this.dataImportacio = dataImportacio;
+		this.nomModel = nomModel;
+		this.preu = preu;
+		this.arreglat = arreglat;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Date getDataImportacio() {
+		return dataImportacio;
+	}
+
+	public String getNomModel() {
+		return nomModel;
+	}
+
+	public double getPreu() {
+		return preu;
+	}
+
+	public boolean isArreglat() {
+		return arreglat;
+	}
+	
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", this.dataImportacio, this.nomModel, this.preu, this.arreglat);
+    }
 	
 }
