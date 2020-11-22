a541e9e1d59089069dfc4982848879afaa32082a
Gartixr <adidumitrica@gmail.com>
2020-11-22 13:58:01 +0100

    Modificar clase VehicleServlet

--- Manteniment/src/manteniment/VehicleServlet.java
+++ Manteniment/src/manteniment/VehicleServlet.java
@@ -1,6 +1,12 @@
 package manteniment;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +19,36 @@ public class VehicleServlet extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		EntityManagerFactory emf = (EntityManagerFactory)getServletContext().getAttribute("emf");
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			String dataImportacio = request.getParameter("dataImportacio");
+			String nomModel = request.getParameter("dataImportacio");
+			String preu = request.getParameter("dataImportacio");
+			String arreglat = request.getParameter("arreglat");
+			if (dataImportacio != null && nomModel != null && preu != null && arreglat != null) {
+				em.getTransaction().begin();
+				em.persist(new Vehicle(dataImportacio, nomModel, preu, arreglat));
+				em.getTransaction().commit();
+			}
+
+				List<Vehicle> guestList = em.createQuery(
+						"SELECT g FROM Vehicle g", Vehicle.class).getResultList();
+				request.setAttribute("guests", guestList);
+				request.getRequestDispatcher("/vehicle.jsp")
+				.forward(request, response);
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			// Close the database connection:
+			if (em.getTransaction().isActive())
+				em.getTransaction().rollback();
+			em.close();
+		}
 	}
 
 
