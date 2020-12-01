d25fafc8fcf30422788b9ebf11aa8a00595f495d
Gartixr <adidumitrica@gmail.com>
2020-12-01 19:13:41 +0100

    Crear metodos necesarios vacios

--- DAM-M06/src/uf2/exercici01/App.java
+++ DAM-M06/src/uf2/exercici01/App.java
@@ -5,12 +5,14 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner teclado =  new Scanner(System.in);
 		Connection con = null; 
 		Driver driver = null;
 		String url = "jdbc:mysql://10.32.24.210:3306";
@@ -49,7 +51,68 @@ public class App {
 					+ "[5] - Eliminar+"
 					+ "[6] - Salir");
 			
+			int input = teclado.nextInt();
+			int selected = 0;
+			
+			switch (input) {
+			case 1:
+				selectAll();
+				break;
+			case 2:
+				System.out.print("ID a mostrar: ");
+				selected = teclado.nextInt();
+				selectOne(selected);
+				break;
+			case 3:
+				System.out.print("ID a actualizar: ");
+				selected = teclado.nextInt();
+				update(selected);
+				break;
+			case 4:
+				insert(selected);
+				break;
+			case 5:
+				System.out.print("ID a eliminar:");
+				selected = teclado.nextInt();
+				delete(selected);
+				break;
+			case 6:
+				System.out.println("Desconectado.");
+				menu = false;
+				break;
+
+			default:
+				System.out.println("Desconectado.");
+				menu = false;
+				break;
+			}
+			
 		}
 
+	}
+
+	private static void delete(int selected) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void insert(int selected) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void update(int selected) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void selectOne(int selected) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void selectAll() {
+		// TODO Auto-generated method stub
+		
 	}
 }
