bcddfe2dadb6d9a9ce1bf0d125b158df6ca416a4
Gartixr <adidumitrica@gmail.com>
2020-12-01 19:56:42 +0100

    Metode selectOne

--- DAM-M06/src/uf2/exercici01/App.java
+++ DAM-M06/src/uf2/exercici01/App.java
@@ -6,14 +6,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 import java.util.Scanner;
 
 public class App {
 
-	static Connection con = null; 
-	
-	public static void main(String[] args) {
+	static Connection con = null;
+	static Statement selectStmt;
+
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 
 		Scanner teclado =  new Scanner(System.in);
@@ -37,24 +37,26 @@ public class App {
 			//Obtenim una connexi6 des de DriverManager
 			con = DriverManager.getConnection(url, usuari, password); 
 			System.out.println("connexió realitzada usant DriverManeger");
+			selectStmt = con.createStatement();
+
 		}
 		catch (SQLException ex) {
 			System.out.println("Error " + ex.getMessage());
 		}
-		
-		
+
+
 		while (menu) {
-			
+
 			System.out.println("[1] - Select all\n"
 					+ "[2] - Select one\n"
 					+ "[3] - Actualizar\n"
 					+ "[4] - Insertar\n"
 					+ "[5] - Eliminar\n"
 					+ "[6] - Salir");
-			
+
 			int input = teclado.nextInt();
 			int selected = 0;
-			
+
 			switch (input) {
 			case 1:
 				selectAll();
@@ -77,64 +79,66 @@ public class App {
 				selected = teclado.nextInt();
 				delete(selected);
 				break;
-			case 6:
-				System.out.println("Desconectado.");
-				menu = false;
-				break;
 
 			default:
 				System.out.println("Desconectado.");
 				menu = false;
+				con.close();
 				break;
 			}
-			
+
 		}
 
 	}
 
 	private static void delete(int selected) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void insert(int selected) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void update(int selected) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	private static void selectOne(int selected) {
-		// TODO Auto-generated method stub
+	private static void selectOne(int selected) throws SQLException {
+
+		ResultSet rs = selectStmt.executeQuery("SELECT * FROM alumnes WHERE id = " + selected);
 		
+		while (rs.next()) {
+			System.out.println("------DADES ALUMNES ID = " + selected + "------");
+			System.out.println("ID: " + rs.getString(1));
+			System.out.println("\tNom: " + rs.getString(2));
+			System.out.println("\tDNI: " + rs.getString(3));
+			System.out.println("\tData naixement: " + rs.getString(4));
+			System.out.println("\tAdreça postal: " + rs.getString(5));
+			System.out.println("\tSexe: " + rs.getString(6));
+			System.out.println("\tCodi postal: " + rs.getString(7));
+			System.out.println("\tPoblació: " + rs.getString(8));
+			System.out.println("------------------------");
+		}
 	}
 
-	private static void selectAll() {
-		// TODO Auto-generated method stub
-		Statement selectStmt;
-		try {
-			selectStmt = con.createStatement();
-			ResultSet rs = selectStmt.executeQuery("SELECT nom FROM alumnes");
-			
-			while (rs.next()) {
-				System.out.println("------------------------");
-				System.out.println("ID: " + rs.getString(1));
-				System.out.println("\tNom: " + rs.getString(2));
-				System.out.println("\tDNI: " + rs.getString(3));
-				System.out.println("\tData naixement: " + rs.getString(4));
-				System.out.println("\tAdreça postal: " + rs.getString(5));
-				System.out.println("\tSexe: " + rs.getString(6));
-				System.out.println("\tCodi postal: " + rs.getString(7));
-				System.out.println("\tPoblació: " + rs.getString(8));
-				System.out.println("------------------------");
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private static void selectAll() throws SQLException {
+
+		ResultSet rs = selectStmt.executeQuery("SELECT nom FROM alumnes");
+
+		while (rs.next()) {
+			System.out.println("------------------------");
+			System.out.println("ID: " + rs.getString(1));
+			System.out.println("\tNom: " + rs.getString(2));
+			System.out.println("\tDNI: " + rs.getString(3));
+			System.out.println("\tData naixement: " + rs.getString(4));
+			System.out.println("\tAdreça postal: " + rs.getString(5));
+			System.out.println("\tSexe: " + rs.getString(6));
+			System.out.println("\tCodi postal: " + rs.getString(7));
+			System.out.println("\tPoblació: " + rs.getString(8));
+			System.out.println("------------------------");
 		}
 	}
 }
